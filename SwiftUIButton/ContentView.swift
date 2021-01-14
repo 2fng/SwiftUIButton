f765e75c3ce4af373dd41496bf108deab1ccff7b
Hua Son Tung <tunghuason@gmail.com>
2021-01-14 21:51:33 +0700

    SwiftUIButton 1.1

--- SwiftUIButton/ContentView.swift
+++ SwiftUIButton/ContentView.swift
@@ -9,8 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Button(action: {
+            print("Delete button was tapped!")
+        }) {
+            Label(
+                title: {
+                    Text("Delete")
+                        .font(.title)
+                        .fontWeight(.semibold)
+                },
+                icon: {
+                    Image(systemName: "trash")
+                        .font(.title)
+                }
+            )
+            .frame(minWidth: 0, maxWidth: .infinity)
             .padding()
+            .background(LinearGradient(gradient: Gradient(colors: [Color("DarkGreen"), Color("LightGreen")]), startPoint: .leading, endPoint: .trailing))
+            .foregroundColor(.white)
+            .cornerRadius(40)
+            .shadow(color: .gray, radius: 10, x: 5, y: 5)
+            
+        }
     }
 }
 
