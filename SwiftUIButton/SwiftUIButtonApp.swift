4d54627b388e377d01abcc3fcdbfd17762387835
Hua Son Tung <tunghuason@gmail.com>
2021-01-14 20:44:59 +0700

    Initial Commit

---
+++ SwiftUIButton/SwiftUIButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIButtonApp.swift
+//  SwiftUIButton
+//
+//  Created by Hua Son Tung on 14/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
